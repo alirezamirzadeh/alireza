6a88b0b2d74aff692968a7d4161d89c8809f4268
alireza <rexalirz72@gmail.com>
2025-04-12 18:02:42 +0330

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 //next.config.mjs
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { withMDX } from 'next-mdx-remote/serialize';
+
+export default withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
  if (process.env.NODE_ENV === 'development') {
    await setupDevPlatform();
  }
 
-export default nextConfig;
